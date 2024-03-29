abbd3a57c33bb818b08ffbfb7b9b0867fd13b637
Jean Julien <reachme.jeanjulien@gmail.com>
2024-03-29 12:37:55 +0100

    fix test by getting the submit button only when it needs to be clicked

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -3,18 +3,16 @@ describe('Sign Up', () => {
     it('Adds person to course ' + (k + 1) + ' / 10', () => {
       cy.visit('/')
 
-      cy.contains('button', 'Submit').then(($submit) => {
-        // type user name into input
-        cy.get('input[name="name"]').click().type('Some Name')
-        // type user email
-        cy.get('input[name="email"]').click().type('some@email.com')
-        // select the "core" department
-        cy.get('select[name="department"]').select('core')
-        // select the "git-it" course
-        cy.get('select[name="course"]').select('git-it')
-        // submit the form
-        cy.wrap($submit).click()
-      })
+      // type username into input
+      cy.get('input[name="name"]').click().type('Some Name')
+      // type user email
+      cy.get('input[name="email"]').click().type('some@email.com')
+      // select the "core" department
+      cy.get('select[name="department"]').select('core')
+      // select the "git-it" course
+      cy.get('select[name="course"]').select('git-it')
+      // submit the form
+      cy.get('[data-x="two"]').click()
       // the "Saved!" message should appear
       cy.get('input[value="Saved!"]').should('be.visible')
       // and the list of registered people should contain the new person
