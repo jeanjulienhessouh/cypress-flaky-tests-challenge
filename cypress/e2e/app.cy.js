6de4442c0f1dde8930a0907c379c37b108f4c06a
Jean Julien <reachme.jeanjulien@gmail.com>
2024-04-22 18:42:11 +0100

    Fix the add person to course test case
    Fix the add person to course test case and update .gitignore file.

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -10,7 +10,7 @@ describe('Sign Up', () => {
     it(`Adds person to course ${k + 1} / 10`, () => {
       cy.visit('/')
 
-      // type user name into input
+      // type username into input
       cy.get('input[name="name"]').click().type('Some Name')
       // type user email
       cy.get('input[name="email"]').click().type('some@email.com')
@@ -19,7 +19,9 @@ describe('Sign Up', () => {
       // select the "git-it" course
       cy.get('select[name="course"]').select('git-it')
       // submit the form
+      cy.intercept('POST', '/students').as('save')
       cy.get('input[type="submit"]').click()
+      cy.wait('@save')
       // the "Saved!" message should appear
       cy.get('input[value="Saved!"]').should('be.visible')
       // and the list of registered people should contain the new person
@@ -29,7 +31,9 @@ describe('Sign Up', () => {
       cy.reload()
       // does this work reliably?
       // why would the same assertion pass before the reload but not now?
-      cy.contains('li', 'Some Name - some@email.com - core - git-it')
+      cy.contains('li', 'Some Name - some@email.com - core - git-it').should('be.visible')
+      
     })
   })
 })
+
